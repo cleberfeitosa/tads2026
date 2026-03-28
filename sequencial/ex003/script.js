3f06a71f7bf3f9224855ea1d24ed8fbd39b43179
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2026-03-28 09:19:03 -0400

    Sequencial Ex004

---
+++ sequencial/ex003/script.js
@@ -0,0 +1,5 @@
+function inicio() {     
+  let nome = window.prompt("Qual é o seu nome?");
+  window.alert(`Olá, ${nome}! Seja bem-vindo(a) ao curso de TADS!`); 
+  window.alert("Iniciando o programa!"); 
+}
